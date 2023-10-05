77c6fe466da029930204d5e2dcf09322bb60e901
MoustafaHassan11 <moustafahassan5544@gmail.com>
2023-10-05 09:13:16 -0700

    task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
-int main(void){
+int main(void)
+
+
+/**
+ * entry point 
+ *
+ * descraption: a c program that prints with puts function 
+ *
+ * return always 0 (success) 
+ */
+
+
+{
 
 
 
