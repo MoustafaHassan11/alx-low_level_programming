6aeccd746381f1f7e48921173806ecf43b7e8e35
MoustafaHassan11 <moustafahassan5544@gmail.com>
2023-10-05 10:17:41 -0700

    task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints use printf function
+ *
+ * Alawys return 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
