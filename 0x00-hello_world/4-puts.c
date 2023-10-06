a8d92d4cb2b9050a5a37f3ed277ff823ed3bf6f9
MoustafaHassan11 <moustafahassan5544@gmail.com>
2023-10-05 19:51:28 -0700

    task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,11 +5,11 @@
  *
  * Description: A C program that prints with put function
  *
- * Return: Alawys 0
+ * Return: Alawys 0 (Success)
 */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-{
+}
