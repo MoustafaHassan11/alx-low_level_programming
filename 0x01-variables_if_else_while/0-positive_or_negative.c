8082158aa96f5803f2e38d247fc6e3befe58cb63
MoustafaHassan11 <moustafahassan5544@gmail.com>
2023-10-08 05:51:43 -0700

    task

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,9 +16,9 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-	printf("%i is postive\n");
+	printf("%i is positive\n");
 	esle if (n < 0)
-	printf("%i is negtive\n");
+	printf("%i is negative\n");
 	esle
 	printf("%i is zero\n");
 	return (0);
