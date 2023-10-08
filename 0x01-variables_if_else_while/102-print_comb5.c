be8e1e6006607c6d6772e75bad06f8297d763190
MoustafaHassan11 <moustafahassan5544@gmail.com>
2023-10-08 16:49:16 -0700

    task

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Write a program that prints all the numbers of base 16 in lowercase,
+ *
+ * Return 0 (success)
+*/
+
+int main(void)
+{
+	int number_1, number_2;
+
+	for (number_1 = 0; number_1 <= 98; number_1++)
+	{
+		number_2 = number_1 + 1;
+		while (number_2 <= 99)
+		{
+			putchar((number_1 / 10) + '0');
+			putchar((number_1 % 10) + '0');
+			putchar(' ');
+			putchar((number_2 / 10) + '0');
+			putchar((number_2 % 10) + '0');
+
+			if (number_1 == 98 && number_2 == 99)
+			{
+				break;
+			}
+			putchar(',');
+			putchar(' ');
+			number_2++;
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
