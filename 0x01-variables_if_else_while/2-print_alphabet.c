01badb1ad94f11fb5d824d96d8d6d3efe96bec33
MoustafaHassan11 <moustafahassan5544@gmail.com>
2023-10-08 09:58:03 -0700

    task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Description: print a to z in lower case and upper case
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	char letters = 'a';
+
+	while (letters <= z)
+	{
+	putchar(letters);
+	letters++;
+	}
+	putchar('\n');
+	return (0);
+
+}
