b74b79821bc4d85adcd019f4bd8ed9d8a40d4a4f
MoustafaHassan11 <moustafahassan5544@gmail.com>
2023-10-08 16:52:00 -0700

    task

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -4,8 +4,9 @@
  * main - Entry point
  *
  * Write a program that prints all the numbers of base 16 in lowercase,
+ *	followed by a new line.
  *
- * Return 0 (success)
+ * Return: 0 (success)
 */
 
 int main(void)
