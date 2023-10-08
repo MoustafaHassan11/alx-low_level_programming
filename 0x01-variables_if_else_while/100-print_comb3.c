9fee4ca25968a7422b244effa2922c0d00a4b740
MoustafaHassan11 <moustafahassan5544@gmail.com>
2023-10-08 16:14:15 -0700

    task

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,34 +3,33 @@
 /**
  * main - Entry point
  *
- * Description: print all possible combination of two digits.
+ * Description: print all possible combinations of two digits.
  *
  * Return: Always 0 (success)
-*/
+ */
 
 int main(void)
 {
 	int digit = 0, digit2;
 
-	while (digit1 <= 9)
+	while (digit <= 9)
 	{
 	digit2 = 0;
 	while (digit2 <= 9)
 	{
-	if (digit1 != digit2 && digit1 < digit2)
+	if (digit != digit2 && digit < digit2)
 	{
-	putchar(digit1 + 48);
-	putchar(digit2 + 48);
-	if (digit1 + digit2 != 17)
+	putchar(digit + '0');
+	putchar(digit2 + '0');
+	if (digit != 8 || digit2 != 9)
 	{
 	putchar(',');
 	putchar(' ');
 	}
-
 	}
 	digit2++;
 	}
-	digit1++;
+	digit++;
 	}
 	putchar('\n');
 
