86f3322355e7b3e0d9c48134f0c89162c9072008
MoustafaHassan11 <moustafahassan5544@gmail.com>
2023-10-10 16:12:45 -0700

    function and nested loop tasks

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - Adds two integers.
+ * @firstNumber: The first integer.
+ * @secondNumber: The second integer.
+ *
+ * Return: The sum of the two integers.
+ */
+int add(int firstNumber, int secondNumber)
+{
+	int result = firstNumber + secondNumber;
+
+	return (result);
+}
