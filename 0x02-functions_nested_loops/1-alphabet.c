9a55f9ff37c8f18aa781b6a31d6bd0e79c1d06f3
MoustafaHassan11 <moustafahassan5544@gmail.com>
2023-10-10 13:30:38 -0700

    function and nested loop tasks

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "mian.h"
+
+/**
+ * print_alphabet - Print the lowercase alphabet followed by a newline
+*/
+
+void print_alphabet(void)
+{
+	for (int ch = 'a'; ch <= 'z'; ch++)
+	_putchar(ch);
+	_putchar('\n');
+}
