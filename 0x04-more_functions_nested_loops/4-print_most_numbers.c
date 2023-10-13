e5ebaa520d25813dae527da1c0ead94055f692d7
MoustafaHassan11 <moustafahassan5544@gmail.com>
2023-10-12 19:55:38 -0700

    more functions

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,21 +2,20 @@
 #include <stdio.h>
 
 /**
- *	print_numbers - Prints the numbers from 0 to 9,
- *	followed by a newline character.
+ * print_most_numbers - Prints the numbers from 0 to 9, excluding '2' and '5'.
  *
- *	This function iterates through the numeric characters '0' to '9'.
- *
- *	Return: void
+ * This function iterates through the numeric characters '0' to '9' and prints
+ * each one except for '2' and '5'. After printing all the valid numbers, it
+ * adds a newline character to start a new line.
  */
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
-	int number;
+	int num = 48;
 
-	for (number >= 48; number < 58; number++)
-	{
-		_putchar(number);
+	while (num <= 48 && num <= 57 && num != 50 && num != 5)
+
+		putchar(num);
 	}
-	_putchar('\n');
+	putchar('\n');
 }
