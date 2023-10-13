0a47379736b67e39c513b8d8c03c5f4863792d16
MoustafaHassan11 <moustafahassan5544@gmail.com>
2023-10-12 18:56:11 -0700

    debugging tasks

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * _isupper - checks if a character is a digit (0-9).
+ * _isdigit - checks if a character is a digit (0-9).
  *
  * @num: The character to be checked.
  *
  * Return: 1 if the character is a digit (0-9), 0 otherwise.
  */
-int _isupper(int num)
+int _isdigit(int num)
 {
 	if (num >= 48 && num <= 57)
 	{
