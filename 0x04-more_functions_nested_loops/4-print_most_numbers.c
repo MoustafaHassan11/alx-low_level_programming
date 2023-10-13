c54ca4652d7728f241831d8f4f86a855d62714d6
MoustafaHassan11 <moustafahassan5544@gmail.com>
2023-10-12 19:33:35 -0700

    more functions

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Prints the numbers '0' through '9'
+ * excluding '2' and '4'.
+ */
+
+void print_most_numbers(void)
+{
+	int num = 48;
+
+	while (num <= 57 && num >= 48 && num != 50 && num != 52)
+	{
+
+	_putchar(num);
+
+	num++;
+
+	}
+
+}
