9fcf97e660629d75e2521c72c42a8b9d9184fea3
MoustafaHassan11 <moustafahassan5544@gmail.com>
2023-10-16 13:36:45 -0700

    pointer and arrays

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -14,14 +14,14 @@
 
 void puts2(char *str)
 {
-
+	int i;
 	for (i = 0 ; str[i] != '\0'; i++)
 	{
 
 	if (i % 2 == 0)
 
-	putchar(str[i]);
+	_putchar(str[i]);
 
 	}
-	putchar('\n');
+	_putchar('\n');
 }
