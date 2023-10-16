8cb7fd6c657f69db78c8daa6f42be59f26b616e7
MoustafaHassan11 <moustafahassan5544@gmail.com>
2023-10-16 15:15:47 -0700

    pointer and arrays

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
+#include <time.h>
 
 /**
  * main - generates random passwords for 101-crackne
@@ -16,7 +16,7 @@ int main(void)
 	srand(time(NULL));
 	while (sum <= 2645)
 	{
-		c = rand() & 128;
+		c = rand() % 128;
 		sum += c;
 		putchar(c);
 	}
