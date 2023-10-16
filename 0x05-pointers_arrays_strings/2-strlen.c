b2e6f6e41566031a9954d199f949aff4fc2aad7c
MoustafaHassan11 <moustafahassan5544@gmail.com>
2023-10-16 07:08:14 -0700

    pointer and arrays

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,25 @@
+#iclude "main.h"
+
+/**
+ * _strlen - Calculate the length of a string.
+ *
+ * This function counts the number of characters
+ * in the string pointed to by 's'
+ * until it reaches the null terminator ('\0').
+ *
+ * @s: A pointer to the string.
+ *
+ * Return: The length of the string.
+ */
+
+int _strlen(char *s)
+{
+
+int length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+		return (length);
+}
