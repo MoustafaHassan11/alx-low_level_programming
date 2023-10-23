5e1d93f363a5c44c4b9db14dbda822c82633d5cb
MoustafaHassan11 <moustafahassan5544@gmail.com>
2023-10-23 13:25:09 -0700

    even more pointers and strings

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,31 +1,29 @@
-#include "main.h"
+# include "main.h"
 
 /**
- * _strchr - Find the first occurrence
- * of a character in a string.
- *
- * @s: Pointer to the string to search.
- * @c: The character to find in the string.
- *
- * Return: Pointer to the first occurrence of `c`
- * in s, or NULL if not found.
- *
- * Description:
- * This function searches for the first occurrence
- * of the character c in the string s
- * It returns a pointer to the character if found
- * or NULL if c is not in the string.
- */
+* _strchr - file memory with a constant byte.
+* @s: pointer to put the constant
+* @c: constant
+* Return: pointer to s
+*/
 
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+
+	int itr;
+
+	for (itr = 0; s[itr] >= '\0'; itr++)
 	{
-		if (*s == c)
+
+		if (s[itr] == c)
 		{
-			return (s);
+
+			return (s + itr);
+
 		}
-		s++;
+
+	return ('\0');
+
 	}
-	return (NULL);
+
 }
